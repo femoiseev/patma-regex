5438425f363a72e683a67e9bdcb8e6643179c7c6
femoiseev <femoiseev@gmail.com>
2020-06-27 14:52:54 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'patma_regex',
+  packages = ['patma_regex'],
+  version = '0.1',
+  license='MIT',
+  description = 'A mini-library for destructive group matching of regexes in Python using recently proposed Structural Pattern Matching (https://www.python.org/dev/peps/pep-0622/).',
+  author = 'Fedor Moiseev',
+  author_email = 'femoiseev@gmail.com',
+  url = 'https://github.com/femoiseev/patma-regex',
+  download_url = 'https://github.com/femoiseev/patma-regex/archive/v_01.tar.gz',
+  keywords = ['pattern matching', 'regex'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3.10',
+  ],
+)
